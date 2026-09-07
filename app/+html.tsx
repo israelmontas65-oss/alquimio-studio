95a7d67d7cf074c43cf06b2601fef2e1f3c72e8d
Israel Montas <israelmontas@gmail.com>
2026-09-06 23:59:16 -0400

    fix(viewport): forzar escala 1.0 y modo standalone nativo para moviles

--- app/+html.tsx
+++ app/+html.tsx
@@ -14,7 +14,7 @@ export default function Root({ children }: PropsWithChildren) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"
         />
 
         {/* Metadatos PWA y Tema */}
